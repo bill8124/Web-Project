8ce3a0fb468a8256c440fd4cae9b9be010f0d69f
Ming-Yuan Jian <b99902067@csie.ntu.edu.tw>
2014-06-18 22:17:37 +0800

    recover original reveal.js code, use Grunt to run

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-
-app.set('port', process.env.PORT || 4500);
-app.use(express.static('.'));
-app.listen(app.get('port'), function(){
-    console.log('Express server started at port ' + app.get('port'));
-});
